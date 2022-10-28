c6c12fe18afa1e330c03e30ee3fce34441741906
Eugene Telyatnik <etelyatn@ford.com>
2022-10-28 19:51:14 +0200

    Init project

---
+++ Source/Holo/Public/Core/HoloGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "HoloGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOLO_API AHoloGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
